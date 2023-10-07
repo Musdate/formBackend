9cdd5d0b43dd8baa70ef39f246aa9fecdacf0b60
Musdate <bastiantroncoso97@gmail.com>
2023-10-07 12:05:28 -0300

    [ADD] DBName & Port

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,9 @@ import { ProductoModule } from './producto/producto.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot( process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME
+    }),
     AuthModule,
     ProductoModule
   ],
--- src/main.ts
+++ src/main.ts
@@ -9,11 +9,11 @@ async function bootstrap() {
 
   app.useGlobalPipes(
     new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
     })
-    );
+  );
     
-  await app.listen(3000);
+  await app.listen( process.env.PORT ?? 3000 );
 }
 bootstrap();
